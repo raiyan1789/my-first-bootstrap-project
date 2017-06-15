a1a3d6b8953ff43b468632b0d06c8aa1eee85f30
ACE User <aceuser@e140-s24.lab.lagcc>
2017-06-14 21:13:23 -0400

    added jquery and bootstrap

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Bootstrap Template 
+# Bootstrap Project
 
 This is a template using Bootstap 3, remember to change git hub repo with 
 git remote set-url origin __________ 
---
+++ js/script.js
@@ -0,0 +1,9 @@
+// var $mainHeader = $('#main-header');
+// $mainHeader.html('hello');
+
+$('#random-gif-btn').click ( function(){
+	$.get( "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho", function( data ) {
+		var img = '<img src=" ' + data.data.fixed_width_small_url + ' ">'
+  		$('#random-gif-container').html(img);
+});
+});
